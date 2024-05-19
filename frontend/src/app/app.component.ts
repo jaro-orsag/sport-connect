e54d0f327e530c4ea9ec15afc38e0e256c4ef804
Jaro Orsag <jorsag@gmail.com>
2024-05-19 17:01:05 +0200

    Extract toolbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenavContainer, MatSidenavContent, MatSidenav } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+    imports: [
+        CommonModule, 
+        RouterOutlet, 
+        RouterLink, 
+        RouterLinkActive, 
+        MatSidenavContainer, 
+        MatSidenavContent, 
+        MatSidenav, 
+        MatToolbar, 
+        MatIcon
+    ],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
-  })
-  export class AppComponent {
+})
+export class AppComponent {
+    @ViewChild(MatSidenav)
+    sidenav!: MatSidenav;
     title = 'routing-app';
-  }
+
+    toggleSideNav() {
+        this.sidenav.toggle();
+    }
+}
