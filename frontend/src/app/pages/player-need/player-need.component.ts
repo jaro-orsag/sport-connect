cac3da0370702e4a960216f2ee87e4202076a4b6
Jaro Orsag <jorsag@gmail.com>
2024-05-20 20:43:05 +0200

    Refactor player-need styles to use BEM notation

--- frontend/src/app/pages/player-need/player-need.component.ts
+++ frontend/src/app/pages/player-need/player-need.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteSelectedEvent, MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -27,6 +27,7 @@ import { Consent } from '../../services/consent';
     imports: [
         FormsModule,
         RouterOutlet,
+        RouterLink,
         MatFormFieldModule,
         MatInputModule,
         MatAutocompleteModule,
