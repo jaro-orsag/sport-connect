e32ac450b8abacd099d8c24a559784d30f203a5a
Jaro Orsag <jorsag@gmail.com>
2024-05-24 15:39:37 +0200

    Add lambda for updating marketing consent for player need

--- lambdas/update-marketing-consent-for-player-need/lambda_function.py
+++ lambdas/update-marketing-consent-for-player-need/lambda_function.py
@@ -4,6 +4,7 @@ import os
 import sys
 import logging
 import pytz
+import json
 
 user_name = os.environ['USER_NAME']
 password = os.environ['PASSWORD']
@@ -31,36 +32,66 @@ def get_current_datetime_in_utc():
     return datetime.now(pytz.utc)
     
 def lambda_handler(event, _): 
+    GRANT_ACTION = "grant"
+    REVOKE_ACTION = "revoke"
+    
     path_parameters = event.get('pathParameters')
-        
     if path_parameters is None or 'uuid' not in path_parameters:
         return {
             'statusCode': 400,
             'body': '\'{uuid}\' path parameter must be defined and not empty.'
         }
-    
     uuid = path_parameters['uuid']
+
+    body_str = event.get('body', '')
+    body_dict = json.loads(body_str)
+    if 'action' not in body_dict.keys() or not body_dict['action']: 
+        return {
+            'statusCode': 400,
+            'body': '\'action\' must be defined.'
+        }
+    action = body_dict['action']
+    if action != GRANT_ACTION and action != REVOKE_ACTION:
+        return {
+            'statusCode': 400,
+            'body': f'\'action\' must be either \'{GRANT_ACTION}\' or \'{REVOKE_ACTION}\'.'
+        }
     
     try:
         utc_now = get_current_datetime_in_utc()
         conn = get_db_connection()
-        with conn.cursor() as cursor:
-            sql = "UPDATE PlayerNeed SET isActive=false, dateDeactivated=(%s) WHERE uuid=(%s)"
-            user_data = (utc_now, uuid)
-            cursor.execute(sql, user_data)
-            conn.commit()
-            cursor.close()
             
         with conn.cursor() as cursor:
-            sql = "UPDATE PlayerNeedConsent SET isActive=false, dateRevoked=(%s) WHERE playerNeedId=(SELECT id FROM PlayerNeed where uuid=(%s)) AND consentId in (1,2,3)"
-            user_data = (utc_now, uuid)
+            dateColumn = 'dateGranted' if action==GRANT_ACTION else 'dateRevoked'
+            sql = f"UPDATE PlayerNeedConsent SET isActive=(%s), {dateColumn}=(%s) WHERE playerNeedId=(SELECT id FROM PlayerNeed where uuid=(%s)) AND consentId=4"
+            isActive = True if action==GRANT_ACTION else False            
+            user_data = (isActive, utc_now, uuid)
             cursor.execute(sql, user_data)
             conn.commit()
+            rowcount = cursor.rowcount
             cursor.close()
             
+            if rowcount > 1:
+                msg = 'more than 1 consent found'
+                logger.warn(msg)
+                return {
+                    'statusCode': 500,
+                    'body': msg
+                }
+           
+            if rowcount == 0:
+                msg = 'consent not found'
+                logger.warn(msg)
+                return {
+                    'statusCode': 404,
+                    'body': msg
+                }
+
             return {
                 'statusCode': 204
             }
+
+
     finally:
         if conn:
             conn.close()
