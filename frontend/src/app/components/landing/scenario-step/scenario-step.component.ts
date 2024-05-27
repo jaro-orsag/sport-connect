719077605d511247534e0123a45fe314a6750e0f
Jaro Orsag <jorsag@gmail.com>
2024-05-27 20:37:54 +0200

    Add draft of responsive landing page

---
+++ frontend/src/app/components/landing/scenario-step/scenario-step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scenario-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './scenario-step.component.html',
+  styleUrl: './scenario-step.component.sass'
+})
+export class ScenarioStepComponent {
+
+}
--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-landing',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.sass'
+    selector: 'app-landing',
+    standalone: true,
+    imports: [MatCardModule, MatButtonModule, MatIconModule, CommonModule],
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.sass'
 })
 export class LandingComponent {
+    Breakpoints = Breakpoints;
+    breakpoint: string = Breakpoints.Large;
 
+    constructor(private responsive: BreakpointObserver) { }
+
+    ngOnInit() {
+
+        this.responsive.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+            .subscribe(result => {
+
+                const breakpoints = result.breakpoints;
+                console.log("result", result);
+                console.log("breakpoints", breakpoints);
+
+                if (breakpoints[Breakpoints.XSmall]) {
+                    this.breakpoint = Breakpoints.XSmall;
+                    console.log("screens matches XSmall");
+                }
+                else if (breakpoints[Breakpoints.Small]) {
+                    this.breakpoint = Breakpoints.Small;
+                    console.log("screens matches Small");
+                }
+                else if (breakpoints[Breakpoints.Medium]) {
+                    this.breakpoint = Breakpoints.Medium;
+                    console.log("screens matches Medium");
+                }
+                else if (breakpoints[Breakpoints.Large]) {
+                    console.log("screens matches Large");
+                    this.breakpoint = Breakpoints.Large;
+                }
+            });
+    }
 }
