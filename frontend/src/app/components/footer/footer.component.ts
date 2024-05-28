e1995b81585e491b40787909cbb6561deda21026
Jaro Orsag <jorsag@gmail.com>
2024-05-28 17:06:24 +0200

    Add contact info to footer

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatGridListModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.sass'
 })
