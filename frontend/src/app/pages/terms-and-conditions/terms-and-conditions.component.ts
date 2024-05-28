c1aa6a7d5772f90028e8a3f8f6dcc342e6e8923f
Jaro Orsag <jorsag@gmail.com>
2024-05-28 15:05:49 +0200

    Consolidate formas and add teams and conditions

--- frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-terms-and-conditions',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './terms-and-conditions.component.html',
   styleUrl: './terms-and-conditions.component.sass'
 })
