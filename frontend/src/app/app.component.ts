6f3b5dccd9bf6fa2f30b3803b4f06217db849837
Jaro Orsag <jorsag@gmail.com>
2024-06-02 14:02:52 +0200

    Workaround not scrolling to top after navigation on iPhone 12 Chrome

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenavContainer, MatSidenavContent, MatSidenav } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -7,18 +7,21 @@ import { MatNavList, MatListItem } from '@angular/material/list';
 import { menuRoutes } from './app.routes';
 import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs';
+import { NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-        CommonModule, 
-        RouterOutlet, 
-        RouterLink, 
-        RouterLinkActive, 
-        MatSidenavContainer, 
-        MatSidenavContent, 
-        MatSidenav, 
+        CommonModule,
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        MatSidenavContainer,
+        MatSidenavContent,
+        MatSidenav,
         MatToolbar,
         MatNavList,
         MatListItem,
@@ -33,6 +36,18 @@ export class AppComponent {
     sidenav!: MatSidenav;
     title = 'routing-app';
     routes = menuRoutes;
+
+    constructor(private router: Router, private viewportScroller: ViewportScroller) { }
+
+    ngOnInit() {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(() => {
+            setTimeout(() => {
+                this.viewportScroller.scrollToPosition([0, 0]);
+            }, 0);
+        });
+    }
 
     toggleSideNav() {
         this.sidenav.toggle();
