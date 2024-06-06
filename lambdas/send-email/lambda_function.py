3d7cdcecab931a9640ef2e246a5945f76e243e0a
Jaro Orsag <jorsag@gmail.com>
2024-06-07 00:35:49 +0200

    Optimize send-email lambda

--- lambdas/send-email/lambda_function.py
+++ lambdas/send-email/lambda_function.py
@@ -2,6 +2,7 @@ import logging
 import boto3
 import json
 from botocore.exceptions import ClientError
+from concurrent.futures import ThreadPoolExecutor
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -11,20 +12,22 @@ SENDER = "futbal-spoluhráč.sk <hladanie@futbal-spoluhrac.sk>"
 RECIPIENT = "futbalspoluhrac@gmail.com"
 AWS_REGION = "us-east-1"
 CHARSET = "UTF-8"
-        
+
+client = boto3.client('ses',region_name=AWS_REGION)
+
 def format_html(beginning, list_of_dicts, ending):
-    result = []
-    for dictionary in list_of_dicts:
-        dict_string = '<br><br>'.join(f"{key}: {make_link_if_email(key, value)}" for key, value in dictionary.items())
-        result.append(dict_string)
-    return f'<html><body>{beginning}<br><br><hr>' + '<hr>'.join(result) + f'<hr><br><br>{ending}</body></html>'
+    result = '<hr>'.join(
+        '<br><br>'.join(f"{key}: {make_link_if_email(key, value)}" for key, value in dictionary.items())
+        for dictionary in list_of_dicts
+    )
+    return f'<html><body>{beginning}<br><br><hr>{result}<hr><br><br>{ending}</body></html>'
 
 def format_text(beginning, list_of_dicts, ending):
-    result = []
-    for dictionary in list_of_dicts:
-        dict_string = '\r\n\r\n'.join(f"{key}: {value}" for key, value in dictionary.items())
-        result.append(dict_string)
-    return f'{beginning}\r\n\r\n' + '\r\n\r\n\r\n'.join(result) + f'\r\n\r\n{ending}'
+    result = '\r\n\r\n\r\n'.join(
+        '\r\n\r\n'.join(f"{key}: {value}" for key, value in dictionary.items())
+        for dictionary in list_of_dicts
+    )
+    return f'{beginning}\r\n\r\n{result}\r\n\r\n{ending}'
 
 def make_link_if_email(key, value):
     return value if key != "email" else f"<a href='mailto: {value}'>{value}</a>"
@@ -38,7 +41,7 @@ def get_link(topicArn, need_type, uuid):
     
     return f"https://{subdomain}.futbal-spoluhrac.sk/{need_type}/{uuid}"
 
-def handle_creation(client, target_email, uuid, need_type, link, subject):
+def handle_creation(target_email, uuid, need_type, link, subject):
     entity = "tím" if need_type == "player-need" else "spoluhráča"
     part1 = f"Gratulujem, v tomto momente sme pre Teba začali hľadať {entity}."
     part2 = f"Hľadanie môžeš kedykoľvek zrušiť na stránke {link}"
@@ -49,30 +52,31 @@ def handle_creation(client, target_email, uuid, need_type, link, subject):
     
     logger.info("going to send confirmation email for %s %s", need_type, uuid)
     try:
-        client.send_email(
-            Destination={
-                'ToAddresses': [
-                    RECIPIENT, # target_email
-                ],
-            },
-            Message={
-                'Body': {
-                    'Html': {
-                        'Charset': CHARSET,
-                        'Data': body_html,
-                    },
-                    'Text': {
-                        'Charset': CHARSET,
-                        'Data': body_text,
-                    },
-                },
-                'Subject': {
-                    'Charset': CHARSET,
-                    'Data': subject,
-                },
-            },
-            Source=SENDER,
-        )
+        logger.info("would send email")
+        # client.send_email(
+        #     Destination={
+        #         'ToAddresses': [
+        #             RECIPIENT, # target_email
+        #         ],
+        #     },
+        #     Message={
+        #         'Body': {
+        #             'Html': {
+        #                 'Charset': CHARSET,
+        #                 'Data': body_html,
+        #             },
+        #             'Text': {
+        #                 'Charset': CHARSET,
+        #                 'Data': body_text,
+        #             },
+        #         },
+        #         'Subject': {
+        #             'Charset': CHARSET,
+        #             'Data': subject,
+        #         },
+        #     },
+        #     Source=SENDER,
+        # )
     except ClientError as e:
         logger.error(e.response['Error']['Message'])
     else:
@@ -80,89 +84,89 @@ def handle_creation(client, target_email, uuid, need_type, link, subject):
     
 
 def lambda_handler(event, _): 
-    client = boto3.client('ses',region_name=AWS_REGION)
     
-    for record in event.get('Records', []):
-        subject = None
-        target_email = None
-        need_type = None
-        uuid = None
-        matches = None
-        
-        try:
-            message = json.loads(record['Sns']['Message'])
-            
-            subject = message['subject']
-            target_email = message['targetEmail']
-            need_type = message['needType']
-            uuid = message['uuid']
-            matches = message['matches']
-            notification_type = message['notificationType']
-            
-            logger.info("received notification of type %s for %s %s", notification_type, need_type, uuid)
-        except KeyError as e:
-            logger.error(f"Key error: {e} - Record: {record}")
-            continue
-        except json.JSONDecodeError as e:
-            logger.error(f"JSON decode error: {e} - Record: {record}")
-            continue
-        except Exception as e:
-            logger.error(f"Unexpected error: {e} - Record: {record}")
-            continue
+    records = event.get('Records', [])
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(process_record, records)
 
-        if need_type == None or uuid == None or target_email == None or subject == None:
-            logger.error("not going to send email. none of the following can be 'None': need_type, target_email, subject, uuid. uuid: %s", uuid)
-            continue
-        link = get_link(record['Sns']['TopicArn'], need_type, uuid)
-        
-        if notification_type == "creation":
-            handle_creation(client, target_email, uuid, need_type, link, subject)
-            continue
-
-        if matches == None or len(matches) == 0:
-            logger.error("not going to send email. matches must not be empty. uuid: %s", uuid)
-            continue
-        
-        end_search = f"Ak už nechceš dostávať ďaľšie emaily, svoje hľadanie môžeš kedykoľvek zrušiť na tomto linku: {link}"
-        message_beginning = f"Neváhaj kontaktovať kohokoľvek zo zoznamu nižšie. {end_search}"
-        message_ending = end_search
-
-        body_html = format_html(message_beginning, matches, message_ending)
-        body_text = format_text(message_beginning, matches, message_ending)
-
-        logger.info("going to send email about match for %s %s", need_type, uuid)
-
-        try:
-            client.send_email(
-                Destination={
-                    'ToAddresses': [
-                        RECIPIENT, # target_email
-                    ],
-                },
-                Message={
-                    'Body': {
-                        'Html': {
-                            'Charset': CHARSET,
-                            'Data': body_html,
-                        },
-                        'Text': {
-                            'Charset': CHARSET,
-                            'Data': body_text,
-                        },
-                    },
-                    'Subject': {
-                        'Charset': CHARSET,
-                        'Data': subject,
-                    },
-                },
-                Source=SENDER,
-            )
-        except ClientError as e:
-            logger.error(e.response['Error']['Message'])
-        else:
-            logger.info("email about match sent for %s %s", need_type, uuid)
-            
     return {
         'statusCode': 200,
         'body': json.dumps('Messages processed successfully')
     }
+    
+def process_record(record):
+
+    try:
+        message = json.loads(record['Sns']['Message'])
+        
+        subject = message['subject']
+        target_email = message['targetEmail']
+        need_type = message['needType']
+        uuid = message['uuid']
+        matches = message['matches']
+        notification_type = message['notificationType']
+        
+        logger.info("received notification of type %s for %s %s", notification_type, need_type, uuid)
+    except KeyError as e:
+        logger.error(f"Key error: {e} - Record: {record}")
+        return
+    except json.JSONDecodeError as e:
+        logger.error(f"JSON decode error: {e} - Record: {record}")
+        return
+    except Exception as e:
+        logger.error(f"Unexpected error: {e} - Record: {record}")
+        return
+
+    if None in [need_type, uuid, target_email, subject]:
+        logger.error("not going to send email. none of the following can be 'None': need_type, target_email, subject, uuid. uuid: %s", uuid)
+        return
+    link = get_link(record['Sns']['TopicArn'], need_type, uuid)
+    
+    if notification_type == "creation":
+        handle_creation(target_email, uuid, need_type, link, subject)
+        return
+
+    if matches == None or len(matches) == 0:
+        logger.error("not going to send email. matches must not be empty. uuid: %s", uuid)
+        return
+    
+    end_search = f"Ak už nechceš dostávať ďaľšie emaily, svoje hľadanie môžeš kedykoľvek zrušiť na tomto linku: {link}"
+    message_beginning = f"Neváhaj kontaktovať kohokoľvek zo zoznamu nižšie. {end_search}"
+    message_ending = end_search
+
+    body_html = format_html(message_beginning, matches, message_ending)
+    body_text = format_text(message_beginning, matches, message_ending)
+
+    logger.info("going to send email about match for %s %s", need_type, uuid)
+
+    try:
+        logger.info("would send email")
+        # client.send_email(
+        #     Destination={
+        #         'ToAddresses': [
+        #             RECIPIENT, # target_email
+        #         ],
+        #     },
+        #     Message={
+        #         'Body': {
+        #             'Html': {
+        #                 'Charset': CHARSET,
+        #                 'Data': body_html,
+        #             },
+        #             'Text': {
+        #                 'Charset': CHARSET,
+        #                 'Data': body_text,
+        #             },
+        #         },
+        #         'Subject': {
+        #             'Charset': CHARSET,
+        #             'Data': subject,
+        #         },
+        #     },
+        #     Source=SENDER,
+        # )
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+    else:
+        logger.info("email about match sent for %s %s", need_type, uuid)
+        
